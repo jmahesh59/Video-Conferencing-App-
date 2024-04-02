44984177ae28a1f1daf25c392359e9d0c659a1d6
mahesh <jadhao.mahesh1009@gmail.com>
2024-04-02 13:12:19 +0530

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className=' flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href={'/'} className='flex items-center gap-1'>
+        <Image 
+         src={'/icons/logo.svg'}
+         width={32}
+         height={32}
+         alt='YOOM logo'
+         className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white  max-sm:hidden'>Yoom</p>
+      </Link>
+      <div className="flex flex-between gap-5">
+          {/** Cleck user management */}
+          <MobileNav/>
+      </div>
+    </nav>
   )
 }
 
