5d7caf469f6d8a279126e7ed6d9287806d14de7f
mahesh <jadhao.mahesh1009@gmail.com>
2024-04-03 14:22:32 +0530

    metting

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 function Navbar() {
   return (
@@ -18,6 +19,9 @@ function Navbar() {
       </Link>
       <div className="flex flex-between gap-5">
           {/** Cleck user management */}
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
           <MobileNav/>
       </div>
     </nav>
